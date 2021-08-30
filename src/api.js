cfc703746426bffd009041236a8011a12be8c51b
LizaSoso4ka <88410393+LizaSoso4ka@users.noreply.github.com>
2021-08-30 20:39:36 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,12 +3,26 @@ const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
+const ips = {
+	"ips": []
+};
 
 router.get("/", (req, res) => {
   res.json({
     hello: "hi!"
   });
 });
+
+  
+router.get("/ip", (req, res) => {
+	
+  const remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+  ips["ips"].push(remoteAddress);
+  
+  res.json(ips);
+});
+
 
 app.use(`/.netlify/functions/api`, router);
 
